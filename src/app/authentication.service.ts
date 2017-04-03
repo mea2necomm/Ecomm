69cf1174f8ac24463e6643efa92e4fa687c86205
Raghunath Thota <rt14j@my.fsu.edu>
2017-04-03 15:58:14 -0400

    Additional functions added to authentication service

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -41,4 +41,57 @@ export class AuthenticationService {
       });
   }
 
+  register(useremail: string, password: string){
+    return this.http.post('/api/login', { email: useremail, password: password })
+      .map((response) => {
+        // login successful if there's a jwt token in the response
+        let token = response.json() && response.json().token;
+        if (token) {
+          this.saveToken(token);
+
+          // return true to indicate successful login
+          return true;
+        } else {
+          // return false to indicate failed login
+          return false;
+        }
+      })
+      .catch((error, caught) => {
+        if (error.status === 401) {
+          console.log(error);
+
+        }
+        return Observable.throw(error);
+      });
+  }
+
+  logout(){
+    localStorage.removeItem('mea2necomm-token')
+  }
+
+  isLoggedIn(){
+    var token = this.getToken();
+    if(token){
+      var payload = JSON.parse(atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+
+    }
+    else{
+      return false;
+    }
+  }
+
+  // Returns the user if user is logged in, or returns false if not logged in
+  currentUser(){
+    if(this.isLoggedIn()){
+      var token = this.getToken;
+      var payload = JSON.parse(atob(token().split('.')[1]));
+      var user = {
+        name: payload.fname,
+        email: payload.email
+      };
+      return user;
+    }
+    else return false;
+  }
 }
